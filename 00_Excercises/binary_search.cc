4e300d3ac8df6a5355a3f51e724d890f598e90cc
mhwu <micw.mm@gmail.com>
2015-03-15 18:22:49 +0800

    binary_search.cc: Added 'binary search for the first occurence' function.

--- 00_Excercises/binary_search.cc
+++ 00_Excercises/binary_search.cc
@@ -51,15 +51,42 @@ void binary_search_with_best_effort(int* a, int i, int j, int x, int* p_k)
 ///
 //
 
+void binary_search_for_the_first(int* a, int i, int j, int x, int* p_k)
+{
+  while (i <= j) {
+    int k = i + ((j - i) >> 1);
+    if (a[k] == x) {
+      if (k > 0 && a[k - 1] == x) {
+        j = k - 1;
+      } else {
+        *p_k = k;
+        break;
+      }
+    } else if (a[k] < x) {
+      *p_k = k;
+      i = k + 1;
+    } else if (a[k] > x) {
+      *p_k = k - 1;
+      j = k - 1;
+    }
+  }
+}
+
+//
+///
+//
+
 int main()
 {
-  int n = 7;
-  int a[] = {1, 2, 3, 5, 6, 8, 10};
-  int k;
+  //int a[] = {1, 2, 3, 3, 3, 5, 6, 6, 6, 6, 8, 10, 10};
+  int a[] = {3, 3, 3, 3, 3, 3};
+  int n = sizeof(a) / sizeof(int);
   int z_x[] = {1, 2, 3, 4, 5, 6, 7, 0, 12};
   for (int i = 0; i < sizeof(z_x) / sizeof(int); i++) {
     int x = z_x[i];
-    binary_search_with_best_effort(a, 0, n - 1, x, &k);
+    int k;
+    //binary_search_with_best_effort(a, 0, n - 1, x, &k);
+    binary_search_for_the_first(a, 0, n - 1, x, &k);
     printf("Search for %d: %d\n", x, k);
   }
 }
